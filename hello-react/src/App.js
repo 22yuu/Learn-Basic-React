e3cfc5993a003e0a9542842700e83f7944eac664
yuu <22yuu@naver.com>
2021-10-29 01:36:03 +0900

    6장 컴포넌트 반복 예제 실습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,44 +1,48 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
+// import ScrollBox from './ScrollBox';
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
 // import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
-// function App() {
-//   // const name = '리액트';
-//   // const style = {
-//   //   backgroundColor: 'black',
-//   //   color: 'aqua',
-//   //   fontSize: '48px',
-//   //   fontWeight: 'bold',
-//   //   padding: 16 // 단위를 생략하면 px로 지정됨.
-//   // }
-//   // return <div style={style}>{name}</div>;
-//   // return <div className="react">{name}</div>
+// import ValidationSample from './ValidationSample';
 
-//   // return (
-//   //   <MyComponent name="React" favoriteNumber={1}>
-//   //     리액트
-//   //   </MyComponent>
-//   // );
+function App() {
+  // const name = '리액트';
+  // const style = {
+  //   backgroundColor: 'black',
+  //   color: 'aqua',
+  //   fontSize: '48px',
+  //   fontWeight: 'bold',
+  //   padding: 16 // 단위를 생략하면 px로 지정됨.
+  // }
+  // return <div style={style}>{name}</div>;
+  // return <div className="react">{name}</div>
+
+  // return (
+  //   <MyComponent name="React" favoriteNumber={1}>
+  //     리액트
+  //   </MyComponent>
+  // );
  
-//   //return <Counter/>
-//   // return <Say/>
-//   // return <EventPractice/>
-//   // return <ValidationSample/>
-//   return <ScrollBox/>
-// }
+  //return <Counter/>
+  // return <Say/>
+  // return <EventPractice/>
+  // return <ValidationSample/>
+  // return <ScrollBox/>
+  return <IterationSample/>
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-      </div>
-    );
-  }
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+//         <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+//       </div>
+//     );
+//   }
+// }
 export default App;
