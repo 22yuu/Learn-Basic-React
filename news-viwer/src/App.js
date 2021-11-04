df9209f73fc8a672f7c6ff609bde97fb200c0995
yuu <22yuu@naver.com>
2021-11-05 01:34:27 +0900

    14장 외부 API를 연동하여 뉴스 뷰어 만들기

---
+++ news-viwer/src/App.js
@@ -0,0 +1,26 @@
+import React,  { useCallback, useState } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
+import NewsList from './components/NewsList';
+import axios from 'axios';
+import Categories from './components/Categories';
+
+const App = () => {
+  // return (
+  //   <Routes>
+  //     <Route path="/:category?" component={NewsPage} />;
+  //   </Routes>
+  // )
+
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
+  return (
+  <>
+  <Categories category={category} onSelect={onSelect}/>
+  <NewsList category={category}/>
+  </>
+  )
+};
+
+export default App;
