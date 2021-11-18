f583be0992b390f0125cc5f0a6824741030058cb
yuu <22yuu@naver.com>
2021-11-19 02:05:29 +0900

    24.1.2

---
+++ blog/blog-frontend/src/App.js
@@ -0,0 +1,21 @@
+import { Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+
+function App() {
+  return (
+    <>
+      <Route component={LoginPage} path='/login' exact />
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={PostPage} path='/@:username/:postId' exact />
+      <Route component={RegisterPage} path="/register" exact />
+      <Route component={WritePage} path="/write" exact />
+
+    </>
+  );
+}
+
+export default App;
